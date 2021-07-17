e7a506c773cac8ca61f9382cc302034b0a789b53
wgcairui <wgcairui@icloud.com>
2021-07-17 09:13:45 +0800

    ./

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,9 +1,39 @@
-import { Framework } from "../dist"
-import { create } from "@midwayjs/mock"
-import { join } from "path"
+import { Framework, IMidwayTcpApplication } from "../src"
+import { createApp, close } from "@midwayjs/mock"
+import { connect } from "net"
+import { once } from "events"
 
 describe("tcpServer", () => {
+
+    let app: IMidwayTcpApplication
+
+    beforeAll(async () => {
+        try {
+            app = await createApp<Framework>(process.cwd(), { port: 3000 }, Framework)
+
+
+        } catch (err) {
+            console.error('test beforeAll error', err);
+            throw err;
+        }
+    })
+
+    afterAll(async () => {
+        await close(app)
+    })
     it("test", async () => {
-        const framework = create<Framework>(join(__dirname, "src"), { port: 3000 })
+
+        const con = connect({ port: 3000 })
+        //con.on("connect")
+        const data = await once(con, "connect")
+
+        console.log(data);
+
+        expect(data).toBe("*")
+
+
+
+        await close(app)
+
     })
 })
